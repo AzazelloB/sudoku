1948e13e90dc07721ee3135dbc5b8a3f8c7bcd19
Eduard <edwardak00@gmail.com>
2023-06-25 21:05:58 +0300

    fix time formatting

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -4,8 +4,8 @@ export const formatTime = (time) => {
   const seconds = Math.floor(time - (hours * 3600) - (minutes * 60));
 
   const hoursStr = hours > 0 ? `${hours}:` : '';
-  const minutesStr = minutes < 10 ? `0${minutes}:` : minutes;
+  const minutesStr = minutes < 10 ? `0${minutes}` : minutes;
   const secondsStr = seconds < 10 ? `0${seconds}` : seconds;
 
-  return `${hoursStr}${minutesStr}${secondsStr}`;
+  return `${hoursStr}${minutesStr}:${secondsStr}`;
 };
