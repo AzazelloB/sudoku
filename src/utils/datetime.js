4969ad004d6c074231e50390e244e33751ac1faf
Eduard <edwardak00@gmail.com>
2023-06-25 20:48:32 +0300

    display hours

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,6 +1,11 @@
 export const formatTime = (time) => {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time - (hours * 3600)) / 60);
+  const seconds = Math.floor(time - (hours * 3600) - (minutes * 60));
 
-  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  const hoursStr = hours > 0 ? `${hours}:` : '';
+  const minutesStr = minutes < 10 ? `0${minutes}:` : minutes;
+  const secondsStr = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${hoursStr}${minutesStr}${secondsStr}`;
 };
