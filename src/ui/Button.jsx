d798886eca3b7113fdfd29e31e4d0aa6595a63f3
Eduard <edwardak00@gmail.com>
2023-06-28 22:54:13 +0300

    fix button style

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,10 +8,11 @@ const Button = (props) => {
       class={classNames(
         props.class,
         'px-4 py-2 rounded-md hover:bg-primary-dark hover:text-white transition-colors duration-200',
+        // TODO use tailwind merge
         {
-          'bg-primary': !props.variant && !props.active,
-          'bg-primary-dark': !props.variant && props.active,
-          'bg-transparent': props.variant === 'tertiary' && !props.active,
+          'bg-primary': !props.variant && !props.active && !props.disabled,
+          'bg-primary-dark': !props.variant && props.active && !props.disabled,
+          'bg-transparent': props.variant === 'tertiary' && !props.active && !props.disabled,
           'text-white': !props.variant,
           'text-black dark:text-white': props.variant === 'tertiary',
           'bg-gray-600 hover:bg-gray-600': props.disabled,
