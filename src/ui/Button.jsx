a5765ddf4fe8a681c6f204905e51685aa66322a3
Eduard <edwardak00@gmail.com>
2023-06-29 22:45:54 +0300

    add tailwind merge

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,22 +1,17 @@
-import classNames from 'classnames';
+import { twMerge } from 'tailwind-merge';
 
 const Button = (props) => {
   return (
     <button
       {...props}
       type="button"
-      class={classNames(
+      class={twMerge(
+        'px-4 py-2 rounded-md transition-colors duration-200',
+        'bg-primary text-white',
+        'hover:bg-primary-dark hover:text-white disabled:bg-gray-600',
+        props.variant === 'tertiary' && 'bg-transparent text-black dark:text-white',
+        props.active && 'bg-primary-dark',
         props.class,
-        'px-4 py-2 rounded-md hover:bg-primary-dark hover:text-white transition-colors duration-200',
-        // TODO use tailwind merge
-        {
-          'bg-primary': !props.variant && !props.active && !props.disabled,
-          'bg-primary-dark': !props.variant && props.active && !props.disabled,
-          'bg-transparent': props.variant === 'tertiary' && !props.active && !props.disabled,
-          'text-white': !props.variant,
-          'text-black dark:text-white': props.variant === 'tertiary',
-          'bg-gray-600 hover:bg-gray-600': props.disabled,
-        },
       )}
       />
   );
