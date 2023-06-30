0aeddbb3e4c9642f544a0a8719c6e51df7c18f13
Eduard <edwardak00@gmail.com>
2023-06-30 19:51:43 +0300

    start using worker and fix build

--- src/workers/boardGenerator.js
+++ src/workers/boardGenerator.js
@@ -1,6 +1,146 @@
 /* eslint-disable no-param-reassign */
-import { mask, solve } from '~/components/Sudoku/board';
+import { difficultyLevels } from '~/constants/difficulty';
+import { shuffleArray } from '~/utils/array';
+
 import { cellsInColumn, cellsInRow } from '~/components/Sudoku/settings';
+
+const isValid = (cells, number, index) => {
+  const col = index % cellsInRow;
+  const row = Math.floor(index / cellsInRow);
+
+  // check row and col
+  for (let i = 0; i < cellsInRow; i++) {
+    const rowIndex = row * cellsInRow + i;
+    const colIndex = col + i * cellsInRow;
+
+    if (rowIndex !== index && cells[rowIndex] === number) {
+      return false;
+    }
+
+    if (colIndex !== index && cells[colIndex] === number) {
+      return false;
+    }
+  }
+
+  // check sudoku area
+  // assuming area is 3x3
+  const areaStartRow = Math.floor(row / 3) * 3;
+  const areaStartCol = Math.floor(col / 3) * 3;
+  const areaEndRow = areaStartRow + 3;
+  const areaEndCol = areaStartCol + 3;
+
+  for (let i = areaStartRow; i < areaEndRow; i++) {
+    for (let j = areaStartCol; j < areaEndCol; j++) {
+      const areaIndex = i * cellsInRow + j;
+
+      if (areaIndex !== index && cells[areaIndex] === number) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+// expects an array filled with nulls only
+const solve = (cells, i = 0) => {
+  if (i === cells.length) {
+    return true;
+  }
+
+  const available = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  while (available.length) {
+    const [value] = available.splice(
+      Math.floor(Math.random() * available.length),
+      1,
+    );
+
+    if (isValid(cells, value, i)) {
+      cells[i] = value;
+
+      if (solve(cells, i + 1)) {
+        return true;
+      }
+    }
+  }
+
+  cells[i] = null;
+  return false;
+};
+
+const getEmptyCellIndex = (cells) => {
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i] === null) {
+      return i;
+    }
+  }
+
+  return null;
+};
+
+const createSolutionCounter = () => {
+  let iter = 0;
+
+  return function countSolutions(cells, count = 0) {
+    iter++;
+
+    if (iter > 1_000_000) {
+      throw new Error('enough');
+    }
+
+    // stop at two since we use this function as a sudoku validator
+    if (count > 1) {
+      return count;
+    }
+
+    const i = getEmptyCellIndex(cells);
+
+    if (i !== null) {
+      for (let num = 1; num <= 9; num++) {
+        if (isValid(cells, num, i)) {
+          cells[i] = num;
+          count = countSolutions(cells, count);
+          cells[i] = null;
+        }
+      }
+    } else {
+      count++;
+    }
+
+    return count;
+  };
+};
+
+const mask = (cells, difficulty) => {
+  try {
+    const countSolutions = createSolutionCounter();
+
+    const masked = cells.slice();
+    const indexes = [...masked.keys()];
+    shuffleArray(indexes);
+
+    const max = masked.length - 1 - difficultyLevels[difficulty];
+    for (let maskedCount = 0; maskedCount <= max;) {
+      const randomIndex = indexes.pop();
+
+      const value = masked[randomIndex];
+
+      masked[randomIndex] = null;
+
+      if (countSolutions(masked.slice()) === 1) {
+        maskedCount++;
+      } else {
+        masked[randomIndex] = value;
+        indexes.unshift(randomIndex);
+      }
+    }
+
+    return masked;
+  } catch (e) {
+    return mask(cells, difficulty);
+  }
+};
 
 onmessage = ({ data: { difficulty } }) => {
   const solved = new Array(cellsInRow * cellsInColumn).fill();
