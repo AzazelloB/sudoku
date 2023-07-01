87604a979503fb7b687cc7417849d2d940cdc1af
Eduard <edwardak00@gmail.com>
2023-07-01 14:48:48 +0300

    use Portal for modals

--- src/ui/Modal/Content.jsx
+++ src/ui/Modal/Content.jsx
@@ -6,6 +6,7 @@ import {
   DialogOverlay,
 } from 'solid-headless';
 import classNames from 'classnames';
+import { Portal } from 'solid-js/web';
 
 import { useModalContext } from '~/context/ModalContext';
 
@@ -17,45 +18,47 @@ const Content = (props) => {
   };
 
   return (
-    <Transition
-      appear
-      show={isOpen()}
-    >
-      <Dialog
-        isOpen
-        class="fixed inset-0 z-30 overflow-y-auto"
-        onClose={closeModal}
+    <Portal>
+      <Transition
+        appear
+        show={console.log(isOpen()) || isOpen()}
       >
-        <div class="h-full flex items-center justify-center">
-          <TransitionChild
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <DialogOverlay class="fixed inset-0 bg-background-dark bg-opacity-50" />
-          </TransitionChild>
+        <Dialog
+          isOpen
+          class="fixed inset-0 z-30 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div class="h-full flex items-center justify-center">
+            <TransitionChild
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <DialogOverlay class="fixed inset-0 bg-background-dark bg-opacity-50" />
+            </TransitionChild>
 
-          <TransitionChild
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <DialogPanel class={classNames(
-              'w-full max-w-md px-8 py-6 overflow-hidden transition-all transform',
-              'bg-background dark:bg-background-dark-accent shadow-xl rounded-2xl',
-            )}>
-              {props.children({ closeModal })}
-            </DialogPanel>
-          </TransitionChild>
-        </div>
-      </Dialog>
-    </Transition>
+            <TransitionChild
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <DialogPanel class={classNames(
+                'w-full max-w-md px-8 py-6 overflow-hidden transition-all transform',
+                'bg-background dark:bg-background-dark-accent shadow-xl rounded-2xl',
+              )}>
+                {props.children({ closeModal })}
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </Dialog>
+      </Transition>
+    </Portal>
   );
 };
 
