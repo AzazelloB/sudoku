44129aab0614a2ebc3923f7b76e5eb0d425472b6
Eduard <edwardak00@gmail.com>
2023-07-05 22:21:31 +0300

    fix board generation time asserting test

--- src/tests/boardGenerator.test.ts
+++ src/tests/boardGenerator.test.ts
@@ -1,36 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {
-  afterEach,
-  beforeEach,
-  describe,
   expect,
   it,
-  vi,
 } from 'vitest';
-// import { onMessage } from '~/workers/boardGenerator';
+import { onMessage } from '~/workers/boardGenerator';
 
-function executeAfterSeconds(seconds: number, func: CallableFunction) {
-  setTimeout(func, 1000 * seconds);
-}
+it('board generator tests', async () => {
+  const ts = Date.now();
+  onMessage({ data: { difficulty: 'hard' } });
 
-const mock = vi.fn(() => console.log('executed'));
-
-describe('board generator tests', () => {
-  beforeEach(() => {
-    vi.useFakeTimers();
-  });
-
-  afterEach(() => {
-    vi.restoreAllMocks();
-  });
-
-  it('test time of execution', async () => {
-    executeAfterSeconds(10, mock);
-    // TODO needs to be called as a worker in a separate thread
-    // onMessage({ data: { difficulty: 'hard' } });
-
-    vi.advanceTimersByTime(5000);
-
-    expect(mock).not.toHaveBeenCalled();
-  });
+  expect(Date.now() - ts).toBeLessThan(8000);
 });
