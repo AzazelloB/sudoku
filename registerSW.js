950c4a62e29d3088b9e29227986e207358e9400e
Eduard <edwardak00@gmail.com>
2023-07-07 20:33:23 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://azazellob.github.io/sudoku/sw.js', { scope: 'https://azazellob.github.io/sudoku/' })})}
