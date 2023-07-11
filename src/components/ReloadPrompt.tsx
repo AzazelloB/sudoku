e6fe88ef5d6b7facbfb58f980116a7aaf22dbc48
Eduard <edwardak00@gmail.com>
2023-07-11 16:38:01 +0300

    fix colors of prompt for light theme

--- src/components/ReloadPrompt.tsx
+++ src/components/ReloadPrompt.tsx
@@ -37,7 +37,7 @@ const ReloadPrompt: Component = () => {
 
   return (
     <Show when={offlineReady() || needRefresh()}>
-      <div class="fixed top-0 z-50 flex w-full items-center justify-between bg-bgfg-800 px-8 py-6">
+      <div class="fixed top-0 z-50 flex w-full items-center justify-between bg-bgfg-200 px-8 py-6 dark:bg-bgfg-800">
         <Show
           fallback={<span>New content available, click on reload button to update.</span>}
           when={offlineReady()}
